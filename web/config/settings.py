4979f4e3dde52991de16c294b890c5612d49f67a
gryroach <gryroach@gmail.com>
2022-04-15 16:50:57 +0300

    Added pagination

--- web/config/settings.py
+++ web/config/settings.py
@@ -56,7 +56,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 30
 }
 
 ROOT_URLCONF = 'config.urls'
