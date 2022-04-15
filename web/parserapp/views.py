038228b7ae7e7225638bd76adc09c60d20255268
gryroach <gryroach@gmail.com>
2022-04-15 19:03:43 +0300

    Refactoring parser_view for POST request

--- web/parserapp/views.py
+++ web/parserapp/views.py
@@ -1,25 +1,40 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter
+from rest_framework.parsers import JSONParser
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework_swagger.views import get_swagger_view
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 from .serializers import DomainSerializer
 from .models import DomainModel
 from .tasks import background_finding_data_from_remote_api
 from .src.filters import DomainFilter
 
 
-@api_view(['GET'])
+validator = URLValidator()
+
+
+@api_view(['GET', 'POST'])
 def parser_view(request):
 
     if request.method == 'GET':
+        return Response("Please, send URL by POST-request", status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
         try:
-            url = request.GET.get('url', None)
-            background_finding_data_from_remote_api.delay(url)
+            url = data['url']
         except Exception as er:
-            return Response({"success": False, "message": str(er)})
-        return Response("Data sent", status=200)
+            return Response({"success": False, "message": str(er)}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            validator(url)
+        except ValidationError as val_er:
+            return Response({"success": False, "message": val_er}, status=status.HTTP_400_BAD_REQUEST)
+        background_finding_data_from_remote_api.delay(url)
+        return Response({"success": True, "message": "Data sent"}, status=status.HTTP_201_CREATED)
 
 
 class DomainView(ListAPIView):
